ce8de5a1e5023e6ce000ae15cceb5c47e2be78c6
Faith calzado <41832061+faithcalzado@users.noreply.github.com>
2018-08-10 23:32:33 +0800

    my first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<link rel="stylesheet" href="loginpage.css">
+<body>
+<link rel="stylesheet" type="text/css" href="loginpage.css">
+<div class="loginbox">
+<h1>WELCOME</h1>
+<h2>Login here</h2>
+Username: <input type "text" name "username" placeholder="Enter Username"><br/><br/>
+Password: <input type "text" name "password" placeholder="Enter Password"><br/><br/>
+<button type"submit" name"submit" value"Login">Login</button></br></br>
+<a href="#">Forgot Password? </a><br></br>
+<a href="#">Sign up</a>
+</div>
+</body>
+
+</head>
+</html>
